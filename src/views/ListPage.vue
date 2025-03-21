0c65fa40d2d19387ea972095fa3a9b8fffc98eed
kimsolhee <solsol6500@naver.com>
2025-03-21 16:34:15 +0900

    Realtime Database 데이터 목록 조회회

--- src/views/ListPage.vue
+++ src/views/ListPage.vue
@@ -1,35 +1,29 @@
 <template>
-  <div>
+  <div style="top: 110px; position: relative;">
     <v-simple-table>
       <template v-slot:default>
         <thead>
           <tr>
-            <th class="text-left">
-              제목
-            </th>
-            <th class="text-left">
-              내용
-            </th>
-            <th class="text-left">
-              작성자
-            </th>
-            <th class="text-left">
-              작성일
-            </th>
+            <th class="text-center">아이디</th>
+            <th class="text-center">제목 </th>
+            <th class="text-center">내용</th>
+            <th class="text-center">작성자</th>
+            <th class="text-center">작성일</th>
           </tr>
         </thead>
         <tbody>
-          <tr v-for="item in poastList" :key="item.name">
+          <tr v-for="item in postList" :key="item.id" class="text-center">
+            <td>{{ item.id }}</td>
             <td>{{ item.title }}</td>
-            <td>{{ item.ctnt }}</td>
+            <td>{{ item.content }}</td>
             <td>{{ item.writer }}</td>
-            <td>{{ item.crtDt }}</td>
+            <td>{{ item.createdAt }}</td>
           </tr>
         </tbody>
       </template>
     </v-simple-table>
-    <div style="display: flex; justify-content: flex-end;">
-      <v-btn depressed color="primary" @click="writePost">
+    <div style="display: flex; justify-content: flex-end;" class="pt-9">
+      <v-btn depressed color="primary" @click="doWrite">
         글쓰기
       </v-btn>
     </div>
@@ -37,17 +31,36 @@
 </template>
 
 <script>
-  export default {
-    name: 'HelloWorld',
-    data () {
-      return {
-        poastList: [],
-      }
+import { ref, onValue } from "firebase/database";
+
+export default {
+  name: 'HelloWorld',
+  created() {
+    this.doList();
+  },
+  data () {
+    return {
+      postList: [],
+    }
+  },
+  methods: {
+    doWrite() {
+      this.$router.push('/write');
     },
-    methods: {
-      writePost() {
-        this.$router.push('/write');
-      }
+    doList() {
+      this.postList = [];
+      const postsRef = ref(this.$db, 'posts');
+      
+      onValue(postsRef, (snapshot) => {
+        const data = snapshot.val();
+        for (const id in data) {
+          this.postList.push({
+            id,
+            ...data[id]
+          });
+        }
+      });
     }
   }
+}
 </script>
