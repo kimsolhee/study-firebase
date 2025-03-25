14acd7efd202c94858dcf4ff0ce370585b85284b
kimsolhee <solsol6500@naver.com>
2025-03-25 13:34:31 +0900

    구글 로그인 추가

--- src/App.vue
+++ src/App.vue
@@ -36,16 +36,65 @@
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
     </v-app-bar>
+    <div style="top: 73px; position: relative; left: 26px;">
+      <h1>Google 로그인 예제 (Vue 2)</h1>
+      <v-btn v-if="!user" color="primary" @click="loginWithGoogle">Google로 로그인</v-btn>
+      <div v-else>
+        <p>환영합니다, {{ user.displayName }} ({{ user.email }})</p>
+        <v-btn color="error" @click="logout">로그아웃</v-btn>
+    </div>
+    </div>
     <router-view />
   </v-app>
 </template>
 
 <script>
+import { GoogleAuthProvider, signInWithPopup, onAuthStateChanged, signOut } from "firebase/auth";
+
 export default {
   name: 'App',
   components: {
   },
-  data: () => ({
-  }),
+  created() {
+    // 로그인 상태 감지
+    onAuthStateChanged(this.$auth, (user) => {
+      if (user) {
+        this.user = user;
+        console.log("로그인 상태:", user);
+      } else {
+        this.user = null;
+        console.log("로그아웃 상태");
+      }
+    });
+  },
+  data () {
+    return {
+      user: null,
+      auth: null
+    }
+  },
+  methods: {
+    loginWithGoogle() {
+      const provider = new GoogleAuthProvider();
+      try {
+        const result = signInWithPopup(this.$auth, provider);
+        this.$nextTick(() => {
+          this.user = result.user;
+          console.log("로그인 성공:", this.user);
+        })
+      } catch (error) {
+        console.error("로그인 실패:", error.message);
+      }
+    },
+    logout() {
+      try {
+        signOut(this.$auth);
+        this.user = null;
+        console.log("로그아웃 성공");
+      } catch (error) {
+        console.error("로그아웃 실패:", error.message);
+      }
+    }
+  }
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -5,8 +5,10 @@ import router from './router';
 Vue.config.productionTip = false
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from "firebase/app"; 
+import { getDatabase } from "firebase/database"; // ✅ Realtime DB용
+import { getFirestore } from "firebase/firestore"; // ✅ Firestore
+import { getAuth } from "firebase/auth"; // ✅ auth
 import vuetify from './plugins/vuetify'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -14,20 +16,25 @@ import vuetify from './plugins/vuetify'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID,
+  measurementId: process.env.VUE_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+const db = getDatabase(app);
+const fs = getFirestore(app);
+const auth = getAuth(app);
 
 Vue.prototype.$db = db;
+Vue.prototype.$fs = fs;
+Vue.prototype.$auth = auth;
 
 new Vue({
   vuetify,
